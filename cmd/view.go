5d5d43c94d29e7e27864d881306e7578517f8776
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-12 08:56:47 +0000

    cleanup

--- cmd/view.go
+++ cmd/view.go
@@ -26,49 +26,57 @@ func (v *ViewCommand) Command() *cobra.Command {
 		Long: `This command reads the data from the specified file and displays it in a sortable table.
 		You can navigate through the table using the next and back buttons.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			db, err := v.dataHandler.OpenDB("./data/canada.db")
-			if err != nil {
-				fmt.Println("Error opening database:", err)
-				return
-			}
-			defer db.Close()
-
-			people, err := v.dataHandler.FetchDataFromDB(db)
-			if err != nil {
-				fmt.Println("Error fetching data from database:", err)
-				return
-			}
-
-			pageSize := 20
-			totalPages := len(people) / pageSize
-			if len(people)%pageSize != 0 {
-				totalPages++
-			}
-
-			for i := 0; i < totalPages; i++ {
-				t := table.NewWriter()
-				t.SetOutputMirror(os.Stdout)
-				t.AppendHeader(table.Row{"ID1", "ID2", "First Name", "Last Name", "Gender", "Birth Place", "Current Place", "Job", "Date"})
-
-				start := i * pageSize
-				end := start + pageSize
-				if end > len(people) {
-					end = len(people)
-				}
-
-				for _, person := range people[start:end] {
-					t.AppendRow([]interface{}{person.ID1, person.ID2, person.FirstName, person.LastName, person.Gender, person.BirthPlace, person.CurrentPlace, person.Job, person.Date})
-				}
-
-				fmt.Printf("Page %d of %d\n", i+1, totalPages)
-				t.Render()
-				if i != totalPages-1 {
-					fmt.Print("Press Enter to continue to the next page...")
-					fmt.Scanln()
-				}
-			}
+			v.RunViewCommand()
 		},
 	}
 
 	return viewCmd
+}
+
+func (v *ViewCommand) RunViewCommand() {
+	db, err := v.dataHandler.OpenDB("./data/canada.db")
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+
+	people, err := v.dataHandler.FetchDataFromDB(db)
+	if err != nil {
+		fmt.Println("Error fetching data from database:", err)
+		return
+	}
+
+	v.RenderTable(people)
+}
+
+func (v *ViewCommand) RenderTable(people []data.Person) {
+	pageSize := 20
+	totalPages := len(people) / pageSize
+	if len(people)%pageSize != 0 {
+		totalPages++
+	}
+
+	for i := 0; i < totalPages; i++ {
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"ID1", "ID2", "First Name", "Last Name", "Gender", "Birth Place", "Current Place", "Job", "Date"})
+
+		start := i * pageSize
+		end := start + pageSize
+		if end > len(people) {
+			end = len(people)
+		}
+
+		for _, person := range people[start:end] {
+			t.AppendRow([]interface{}{person.ID1, person.ID2, person.FirstName, person.LastName, person.Gender, person.BirthPlace, person.CurrentPlace, person.Job, person.Date})
+		}
+
+		fmt.Printf("Page %d of %d\n", i+1, totalPages)
+		t.Render()
+		if i != totalPages-1 {
+			fmt.Print("Press Enter to continue to the next page...")
+			fmt.Scanln()
+		}
+	}
 }
